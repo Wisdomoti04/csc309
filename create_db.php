2de8cf6f120a1ea3d4a969afdae490c112035a89
Wisdomoti04 <100049990+Wisdomoti04@users.noreply.github.com>
2023-07-18 07:38:36 +0100

    Update create_db.php

--- create_db.php
+++ create_db.php
@@ -3,24 +3,33 @@
     $server = 'localhost'; // 127.0.0.1
     $username = 'root';
     $password = '';
+    $db = 'csc309';
 
     // Open a new connection
-    $con = new mysqli($server, $username, $password);
+    $con = new mysqli($server, $username, $password, $db);
+
 
     // Check connection
     if ($con->connect_error){
         die("Connection failed: " . $con->connect_error);
     }
 
-    // Create Database
-    $sql = 'CREATE DATABASE csc309';
+    // Create a Table
+    $sql = 'CREATE TABLE users (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        firstname VARCHAR(30) NOT NULL,
+        lastname VARCHAR(30) NOT NULL,
+        email VARCHAR(50) NOT NULL UNIQUE,
+        gender CHAR(1) NOT NULL,
+        date_of_birth DATE NOT NULL
+    )';
 
     $result = $con->query($sql);
 
     if ($result === true) {
-        echo 'Database creation successful';
+        echo 'Table created successfully';
     } else {
-        echo 'Error creating database: ' . $con->error;
+        echo 'Error creating creating2: ' . $con->error;
     }
 
     // Close connection
